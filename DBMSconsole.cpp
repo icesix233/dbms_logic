2d3ee1c213c7b328b439671d5ecec7a97d088acd
alexwood2333 <myalexwood@gmail.com>
2022-05-15 20:21:04 +0800

    add test for deleting field

--- DBMSconsole.cpp
+++ DBMSconsole.cpp
@@ -76,11 +76,12 @@ void testtable() {
 	//studentTable.printTable(); // 打印表
 
 	// 字段操作
-	studentTable.changeField(1, "StudentName");  // 将第二列（Name）改为StudentName
-	// 在第二列（Name）后增加一列性别。注意增加后所有这列的数据都为空字符串。
-	studentTable.addField(1, "GENDER", "string");
-	// 修改第二行第三列（性别)为MALE
-	studentTable.changeRow(1, 2, "Male");
+	//studentTable.changeField(1, "StudentName");  // 将第二列（Name）改为StudentName
+	//// 在第二列（Name）后增加一列性别。注意增加后所有这列的数据都为空字符串。
+	//studentTable.addField(1, "GENDER", "string");
+	//// 修改第二行第三列（性别)为MALE
+	//studentTable.changeRow(1, 2, "Male");
+	//studentTable.deleteField(1);
 	studentTable.printTable();
 
 	studentTable.saveData(); // 保存文件内容
